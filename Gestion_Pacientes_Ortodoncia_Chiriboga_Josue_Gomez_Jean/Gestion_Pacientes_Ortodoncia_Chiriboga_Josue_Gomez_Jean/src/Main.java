e91dcabd8c86ff01b4442601b447b2ec34ed5e58
Jean Carlos G <168456517+JeanCarlosG2001@users.noreply.github.com>
2024-11-30 18:30:10 -0500

    Update Main.java
    Menú do switch while

--- Gestion_Pacientes_Ortodoncia_Chiriboga_Josue_Gomez_Jean/Gestion_Pacientes_Ortodoncia_Chiriboga_Josue_Gomez_Jean/src/Main.java
+++ Gestion_Pacientes_Ortodoncia_Chiriboga_Josue_Gomez_Jean/Gestion_Pacientes_Ortodoncia_Chiriboga_Josue_Gomez_Jean/src/Main.java
@@ -10,12 +10,38 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
+        int opcion;
+        Paciente paciente = null;
 
+        while (true) {
+            System.out.println("----------Gestion de Pacientes de Ortodoncia----------");
+            System.out.println("1. Registrar paciente");
+            System.out.println("2. Mostrar información paciente");
+            System.out.println("3. Salir");
+
+            System.out.print("Ingrese su opción: ");
+            opcion = sc.nextInt();
+            sc.nextLine();
+
+            switch (opcion) {
+                case 1:
+                    paciente = registrarPaciente(sc);
+                    break;
+                case 2:
+                    mostrarInformacionPaciente(paciente);
+                    break;
+                case 3:
+                    System.out.println("Adiós");
+                    return;
+                default:
+                    System.out.println("Opción inválida. Por favor, inténtelo de nuevo.");
+            }
+        }
+    }
+
+    public static Paciente registrarPaciente(Scanner sc) {
         //REGISTRO DE PACIENTE
-        System.out.println("----------Gestion de Pacientes de Ortodoncia----------");
-
         System.out.println("\nIngrese ID del paciente:");
         int idPaciente = sc.nextInt();
         sc.nextLine();
@@ -34,7 +60,7 @@ public class Main {
         System.out.println("Anio:");
         int anioUltimo = sc.nextInt();
 
-        Fecha fechaUltimoControl = new Fecha(diaUltimo,mesUltimo,anioUltimo);
+        Fecha fechaUltimoControl = new Fecha(diaUltimo, mesUltimo, anioUltimo);
 
         System.out.println("Fecha Proximo Control:");
         System.out.println("Dia:");
@@ -45,7 +71,7 @@ public class Main {
         int anioProximo = sc.nextInt();
         sc.nextLine();
 
-        Fecha fechaProximoControl = new Fecha(diaProximo,mesProximo,anioProximo);
+        Fecha fechaProximoControl = new Fecha(diaProximo, mesProximo, anioProximo);
 
         System.out.println("Grupo del paciente (G1/G2/G3/G4):");
         String grupo = sc.nextLine();
@@ -58,8 +84,7 @@ public class Main {
         String nombreDoctora = sc.nextLine();
         System.out.println("Apellido:");
         String apellidoDoctora = sc.nextLine();
-        Doctor doctora = new Doctor(idDoctora,nombreDoctora,apellidoDoctora);
-
+        Doctor doctora = new Doctor(idDoctora, nombreDoctora, apellidoDoctora);
 
         System.out.println("Asistencia del paciente (Asiste/No Asiste):");
         String asistencia = sc.nextLine();
@@ -71,17 +96,17 @@ public class Main {
         double valorControl = sc.nextDouble();
         sc.nextLine();
 
-        Paciente paciente = new Paciente(idPaciente,nombrePaciente,apellidoPaciente,fechaUltimoControl,fechaProximoControl,grupo,doctora,asistencia,estadoControl,valorControl);
+        Paciente paciente = new Paciente(idPaciente, nombrePaciente, apellidoPaciente, fechaUltimoControl, fechaProximoControl, grupo, doctora, asistencia, estadoControl, valorControl);
 
-        //Mostrar Informacion Paciente
-        System.out.println("\nDATOS DEL PACIENTE");
-        System.out.println(paciente);
+        return paciente;
+    }
 
-
-
-        sc.close();
-
-
-
+    public static void mostrarInformacionPaciente(Paciente paciente) {
+        if (paciente != null) {
+            System.out.println("\nDATOS DEL PACIENTE");
+            System.out.println(paciente);
+        } else {
+            System.out.println("No hay paciente registrado");
+        }
     }
 }
